65ca5f359a85327460408175927e2ad0fd16562d
FalasFry <william.persson@live.se>
2023-04-12 14:27:39 +0200

    works for the 5 questions RN

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,28 @@
 <template>
   <div>
-    
+    <h2>Welcome To the practice driving quiz</h2>
+    <QuizApp/>
   </div>
 </template>
 
 <script>
+import QuizApp from './components/QuizApplet.vue'
 
+export default {
+  name: 'App',
+  components: {
+    QuizApp
+  },data() {
+    return {
+      currentComponent: null
+    }
+  },
+  methods: {
+    loadComponent() {
+      this.currentComponent = QuizApp
+    }
+  }
+}
 </script>
 
 <style>
@@ -16,5 +33,21 @@
   text-align: center;
   color: #7592af;
   margin-top: 60px;
+}
+
+button {
+  font-size: 20px;
+  text-align: center;
+  background-color: #0000002b;
+  border: 10px;
+  border-radius: 10px;
+  margin: 5px;
+  color: #7592af;
+  box-shadow: 5px;
+  padding: 15px;
+}
+
+body {
+  background-color: #252525;
 }
 </style>
---
+++ src/components/QuizApplet.vue
@@ -0,0 +1,81 @@
+<template>
+    <div>
+      <button v-if="!isStarted" @click="StartQuiz">Start Quiz</button>
+      <button v-if="isStarted">End Quiz</button>
+
+      <div v-if="isStarted" class="QuizGame">
+        <h2>Welcome To The Quiz</h2>
+        
+        <h4> Rätta Svar: {{ score }} / {{ jsonData.length }} </h4>
+        <h3>{{ jsonData[curQuestion].question }}</h3>
+
+
+        <div class="questionDisplay">
+            <div v-for="(answer, index) in jsonData[curQuestion].options" :key="index">
+                <button @click="CheckAnswer(answer)">{{ answer }}</button>
+            </div>
+        </div>
+      </div>
+    </div>
+</template>
+  
+<script>
+  import jsonFile from '../assets/questions.json'
+  
+  export default {
+      name: "QuizApp",
+      methods: {
+        StartQuiz:function(){
+          this.jsonData = jsonFile.questions;
+          this.curQuestion = 0;
+          this.score = 0;
+          this.Shuffle(this.jsonData);
+          this.wrongAnswers = [];
+
+          this.isStarted = true;
+        },
+        CheckAnswer:function(option){
+            if(this.jsonData[this.curQuestion].answer === option){
+                console.log('CORRECT');
+                this.score+=1;
+            }else{
+                console.log('WRONG');
+            }
+            
+            if(this.curQuestion < this.jsonData.length-1){
+              this.curQuestion+=1;
+            }
+        },
+        Shuffle:function(array) {
+          for (var i = array.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+          }
+        }
+
+      },
+      data() {
+        return{
+            jsonData: Object,
+            curQuestion: Number,
+            score: Number,
+            isStarted: false,
+            wrongAnswers: Array,
+        }
+      }
+  }
+</script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  
+  .questionDisplay {
+    display: flex;
+    text-align: center;
+    justify-content: center;
+  }
+  
+</style>
+  
