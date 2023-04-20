f0cd689f37dfec1c0afd47fba863bd4e024dc53f
FalasFry <william.persson@live.se>
2023-04-20 13:25:01 +0200

    asd

--- src/components/QuizApplet.vue
+++ src/components/QuizApplet.vue
@@ -1,8 +1,5 @@
 <template>
     <div>
-      <header>
-        <h1>     </h1>
-      </header>
       <h1 v-if="!isStarted">Lär Dig Teori!</h1>
       <button v-if="!isStarted" @click="StartQuiz">Starta Quiz</button>
 
